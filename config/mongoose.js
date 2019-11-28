cd9685bd3c3ea4ec3ddbfdb83a240aa0aba42383
Immanuel Pandiangan <nuelpan@gmail.com>
2019-11-28 21:58:10 +0700

    add bored feature

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.URL_DB, {
-  useCreateIndex: true,
+// mongoose.connect(process.env.URL_DB, {
+mongoose.connect("mongodb://localhost:27017/test", {
+    useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const user = require('./userRoute')
-
+const bored = require('./boredRoute')
 
 router.use('/user', user)
+router.use('/bored', bored)
 
 
 
