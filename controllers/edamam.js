55abd5a6a419f08e2fae168ed3d8162a5d6716da
Amil Hasbala <amilhasbala@gmail.com>
2019-11-29 15:58:40 +0700

    terakhir

--- controllers/edamam.js
+++ controllers/edamam.js
@@ -5,7 +5,7 @@ class EdamamController {
         let recipe = req.params.recipe
         if (recipe) {
             Edamam({
-                url: `search?q=${req.params.recipe}&app_id=3f836cdc&app_key=b71525fc53bf070d830fd1fae60e5362`
+                url: `search?q=${req.params.recipe}&app_id=926039ee&app_key=a3fd178991cf09316d3d8fd0b2e2d48d`
             })
                 .then(recipe => {
                     res.status(200).json(recipe.data)
